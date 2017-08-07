b76c359940c86647b89aa57baa600e4c73693ee5
nishajagadesh <30690268+nishajagadesh@users.noreply.github.com>
2017-08-07 14:42:12 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -23,3 +23,19 @@ button.onclick = function(){
   request.open('GET','http://mailnisha96.imad.hasura-app.io/counter', true);
   request.send(null);
 };
+
+//Submit Name
+var nameInput = document.getElementById('name');
+var name = nameInput.value;
+var submit = document.getElementById('submit_btn');
+submit.onclick = function(){
+    //make request to server and send name
+    var names = ['name1','name2','name3','name4'];
+    var list='';
+    for (var i=0; i<names.length; i++){
+        list == '<li>' + names[i] + '</li'>; 
+    }
+    var ul = document.getElementById('nameList');
+    ul.innerHTML = list;
+    //capture names and render as a list
+};
