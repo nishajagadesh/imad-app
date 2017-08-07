857e9e96af79f798c6b803043ee6cdebeeb3c6bb
nishajagadesh <30690268+nishajagadesh@users.noreply.github.com>
2017-08-07 12:58:55 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -15,8 +15,8 @@ button.onclick = function(){
    };
    //render the variable in the correct span
   // counter = counter +1;
-  // var span = document.getElementById('count');
-  // span.innerHTML = counter.toString();
+   var span = document.getElementById('count');
+   span.innerHTML = counter.toString();
   
   //MAKE a request
   request.open('GET','http://mailnisha96.imad.hasura-app.io/counter', true);
