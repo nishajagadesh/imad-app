e941e57d2108ba250f6a6a8956e4f755616f42f4
nishajagadesh <30690268+nishajagadesh@users.noreply.github.com>
2017-08-08 17:54:18 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -36,20 +36,19 @@ app.get('/test-db', function(req,res){
     });
 });
 app.get('/articles/:articleName', function (req, res){
-   pool.query("SELECT * from article where title =\" + req.params.articleName), function (err,result){
+   pool.query("SELECT * from article WHERE title =" + req.params.articleName, function (err,result){
        if (err){
            res.status(500).send(err,toString());
        } else {
-           if (result.rows.length == 0){
+           if (result.rows.length === 0){
                res.status(404).send("Article not found");
            } else {
                var articleData = results.rows[0];
                res.send(createTemplate(articleData));
            }
        }
-   }
-    
-});
+   });
+ });
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
