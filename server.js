7cf43e18bf6f87b9b803240392c49b0ca2971813
nishajagadesh <30690268+nishajagadesh@users.noreply.github.com>
2017-08-17 18:42:55 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -77,6 +77,13 @@ app.post('/login', function(req,res){
     });
 });
 
+app.get('/check-login', function(req,res){
+    if(req.session && res.session.auth && req.session.auth.userId){
+        res.send('You are logged in' + req.session.auth.userId.toString());
+    } else{
+        res.send('You are not logged in');
+    }
+});
 var pool = new Pool(config);
 
 //var counter = 0;
