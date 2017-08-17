708bb7d78bc52b50aa669212f210c849c9e5869f
nishajagadesh <30690268+nishajagadesh@users.noreply.github.com>
2017-08-17 18:23:19 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ var config = {
 };
 var app = express();
 app.use(morgan('combined'));
-app.user(bodyParser.json());
+app.use(bodyParser.json());
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
