8d968766bd607ea372f5d6cf94cdaff8a1e4a273
nishajagadesh <30690268+nishajagadesh@users.noreply.github.com>
2017-08-17 18:07:49 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,41 +1,57 @@
-var button = document.getElementById('counter');
-button.onclick = function(){
+//var button = document.getElementById('counter');
+//button.onclick = function(){
    //make a request to the counter endpoint
-   var request = new XMLHttpRequest();
+  // var request = new XMLHttpRequest();
   //CREATE a request
    //capture and store it in a variable
-   request.onreadystatechange = function(){
-       if(request.readyState === XMLHttpRequest.DONE){
+   //request.onreadystatechange = function(){
+     //  if(request.readyState === XMLHttpRequest.DONE){
         //Take some action   
-        if(request.status ===200){
-            var counter = request.responseText;
-            var span = document.getElementById('count');
-            span.innerHTML = counter.toString();
-        }
-       }
+       // if(request.status ===200){
+         //   var counter = request.responseText;
+           // var span = document.getElementById('count');
+            //span.innerHTML = counter.toString();
+//        }
+ //      }
        //Not yet done
-   };
+ //  };
    //render the variable in the correct span
   // counter = counter +1;
 
   
   //MAKE a request
-  request.open('GET','http://mailnisha96.imad.hasura-app.io/counter', true);
-  request.send(null);
-};
+  //request.open('GET','http://mailnisha96.imad.hasura-app.io/counter', true);
+  //request.send(null);
+//};
 
 //Submit Name
-var nameInput = document.getElementById('name');
-var name = nameInput.value;
+//var nameInput = document.getElementById('name');
+//var name = nameInput.value;
 var submit = document.getElementById('submit_btn');
 submit.onclick = function(){
-    //make request to server and send name
-    var names = ['name1','name2','name3','name4'];
-    var list='';
-    for (var i=0; i<names.length; i++){
-        list += '<li>' + names[i] + '</li>'; 
-    }
-    var ul = document.getElementById('nameList');
-    ul.innerHTML = list;
-    //capture names and render as a list
-};
+   var request = new XMLHttpRequest();
+  //CREATE a request
+   //capture and store it in a variable
+   request.onreadystatechange = function(){
+     if(request.readyState === XMLHttpRequest.DONE){
+       //Take some action   
+         if(request.status ===200){
+            console.log('user logged in');
+            alert('Logged in successfully');
+         } else {
+            if(request.status === 403){
+                alert('username/password is incorrect');
+            } 
+            else {
+                alert('something went wrong on the server');
+            }
+         }
+     };
+
+var username = document.getElementById('username').value;
+var password = document.getElementById('password').value;
+console.log(username);
+console.log(password);
+request.open('POST', 'http://mailnisha96.imad.hasura-app.io/login', true);
+request.setRequestHeader('Content-Type', 'application/json', 
+request.send(JSON.stringify({username: username, password: password}));
